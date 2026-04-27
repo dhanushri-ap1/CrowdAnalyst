3ba46029eec217f578ce54f7c4bbfe0bf4732ac8
dhanushri <dhanushri.anithaprasanna1@gmail.com>
2026-04-27 21:05:25 +0530

    Coordinates updated

--- backend/seed.js
+++ backend/seed.js
@@ -12,33 +12,62 @@ const seedPlaces = async () => {
 
     await Place.insertMany([
       {
-        name: "Main Library",
-        category: "library",
-        location: {
-          type: "Point",
-          coordinates: [80.2206, 13.0827]
-        }
-      },
-      {
-        name: "Food Court",
+        name: "Main Canteen",
         category: "canteen",
         location: {
           type: "Point",
-          coordinates: [80.2212, 13.0830]
-        }
+          coordinates: [76.9043074498533, 10.901428180909283]
+        },
+        connections: ["IT Canteen", "Library"]
       },
       {
-        name: "Parking Lot",
-        category: "parking",
+        name: "IT Canteen",
+        category: "canteen",
         location: {
           type: "Point",
-          coordinates: [80.2220, 13.0820]
-        }
+          coordinates: [76.89811962393252, 10.90508918779489]
+        },
+        connections: ["Main Canteen", "MBA Canteen"]
+      },
+      {
+        name: "MBA Canteen",
+        category: "canteen",
+        location: {
+          type: "Point",
+          coordinates: [76.89811962393252, 10.90508918779489]
+        },
+        connections: ["IT Canteen"]
+      },
+      {
+        name: "Sports Ground",
+        category: "sports",
+        location: {
+          type: "Point",
+          coordinates: [76.90293988666609, 10.902890933488104]
+        },
+        connections: ["Auditorium"]
+      },
+      {
+        name: "Library",
+        category: "library",
+        location: {
+          type: "Point",
+          coordinates: [76.89908367646774, 10.904479269169551]
+        },
+        connections: ["Main Canteen", "Auditorium"]
+      },
+      {
+        name: "Auditorium",
+        category: "auditorium",
+        location: {
+          type: "Point",
+          coordinates: [76.90296576726746, 10.903951942680145]
+        },
+        connections: ["Library", "Sports Ground"]
       }
     ]);
 
-    console.log("Sample places inserted");
-
+    console.log("FlowGuard places inserted");
     process.exit();
   } catch (error) {
     console.log(error);
